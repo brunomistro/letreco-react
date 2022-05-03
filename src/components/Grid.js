16b8dcb8bb85b9408f49638d3484afad912588bf
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-05-03 17:38:09 -0300

    dinymic typing

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,9 @@ export default function Grid({ guesses, currrentGuess, turn }) {
 	return (
 		<div>
 			{guesses.map((guess, index) => {
+				if(turn === index) {
+					return <Row key={index} currrentGuess={currrentGuess} />
+				}
 				return <Row key={index} guess={guess} />
 			})}
 		</div>
